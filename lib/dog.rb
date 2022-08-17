ace21619dce9f95976dc701a09253eccff523305
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-08-18 00:18:22 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
+require "pry"
 class Dog
+    #setter method
+    def name=(dog_name)
+    #instance variable is passed a value passed as an argument in setter method
+        @this_dogs_name = dog_name
+    end
 
+    #getter method
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+binding.pry
